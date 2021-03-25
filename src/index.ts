c1202294f72fca284dda6d4763477f50ca355a83
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 22:29:27 +0900

    import cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as bodyParser from "body-parser";
 import jwt, { JsonWebTokenError } from 'jsonwebtoken'
+import cors from 'cors'
 
 import auth from './controllers/auth'
 
@@ -9,12 +10,15 @@ const env = process.env
 const PORT = env.PORT || 5000
 const router = express.Router();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Token, Accept")
-  res.header("Access-Control-Expose-Headers", "*")
-  next()
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Token, Accept")
+//   res.header("Access-Control-Expose-Headers", "*")
+//   next()
+// })
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
