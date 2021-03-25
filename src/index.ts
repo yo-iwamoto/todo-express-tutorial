a99a8f15a17884e52bb2c39979325a7872b657dc
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 22:52:02 +0900

    change setting of origin to array

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import * as bodyParser from "body-parser";
 import jwt, { JsonWebTokenError } from 'jsonwebtoken'
-import cors = require('cors')
+// import cors = require('cors')
 
 import auth from './controllers/auth'
 
@@ -10,15 +10,19 @@ const env = process.env
 const PORT = env.PORT || 5000
 const router = express.Router();
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Token, Accept")
-//   res.header("Access-Control-Expose-Headers", "*")
-//   next()
-// })
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://leisurely-todo-9ckjnv9h7-you-5805.vercel.app", "https://leisurely-todo.vercel.app", "http://localhost:8080"]
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Token, Accept")
+  res.header("Access-Control-Expose-Headers", "*")
+  next()
+})
 
-app.use(cors())
+// app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
