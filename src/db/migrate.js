160e551dcdba44514a69c306ce68fa48d28d4371
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 21:03:49 +0900

    migration file

---
+++ src/db/migrate.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+const env = process.env
+const production = env.NODE_ENV === 'production'
+
+const connection = mysql.createConnection({
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASS,
+  database: env.DB_DATABASE
+})
+
+connection.query('CREATE TABLE user (id int auto_increment primary key not null, uid varchar(200) not null, created_at datetime default current_timestamp, updated_at timestamp default current_timestamp on update current_timestamp)')
