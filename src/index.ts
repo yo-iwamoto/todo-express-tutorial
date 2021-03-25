5242a4540c483bd9f3feb1c66badc48e403e8d72
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 09:44:40 +0900

    change

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import * as bodyParser from "body-parser";
 
 const path = require('path')
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 const app = express();
 const router = express.Router();
@@ -17,7 +17,7 @@ router.get("/", (req: express.Request, res: express.Response) => {
 
 app.use(router);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`listening on port ${ port }`);
 });
 
