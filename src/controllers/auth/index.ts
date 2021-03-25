1c37bd4fb9c2acc5f1694dda6f9dd28abfbea166
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 20:47:26 +0900

    register and sync

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,18 +1,54 @@
 import * as Express from 'express'
-import jwt from 'jsonwebtoken'
+import jwt, { JsonWebTokenError } from 'jsonwebtoken'
+import { MysqlError } from 'mysql'
+import { connection } from '../../db/database'
 
 const router = Express.Router({ mergeParams: true })
 
 const SECRET_KEY = process.env.SECRET_KEY
+const createUserSql = 'INSERT INTO user VALUE ()'
+const selectLastIdSql = 'SELECT id FROM user ORDER BY id ASC LIMIT 1'
+
+interface Payload {
+  id: number;
+}
 
 router.post('/register', (req: Express.Request, res: Express.Response) => {
-  res.json([
-    { message: 'register' }
-  ])
+  // 最後のUserのidに+1したidでUserを作成し，SQLを1文で済ませる．
+  connection.query(createUserSql, (err: MysqlError) => {
+    if (err) {
+      res.send(500)
+    }
+  })
+  connection.query(selectLastIdSql, (err: MysqlError, results: any) => {
+    if (err) {
+      res.send(500)
+    } else {
+      const id: number = results[0].id
+      const payload: Payload = { id: id }
+      const accessToken = jwt.sign(payload, SECRET_KEY)
+      res.setHeader('Access-Token', accessToken)
+      res.send(200)
+    }
+  })
 })
 
-router.post('/signup', (req: Express.Request, res: Express.Response) => {
-  res.send('signup')
+const updateUserByIdSql = (id: number, uid: number): string => `UPDATE user SET uid = ${uid} WHERE id = ${id}`
+
+// firebase uidの紐付け
+router.post('/sync', (req: Express.Request, res: Express.Response) => {
+  const accessToken = req.header('access-token')
+  const decoded = jwt.verify(accessToken, SECRET_KEY) as Payload
+  const id = decoded.id
+  const uid = req.body.uid
+  connection.query(updateUserByIdSql(id, uid), (err: MysqlError, results: any) => {
+    if (err) {
+      res.send(500)
+    } else {
+      console.log(results)
+      res.send(200)
+    }
+  })
 })
 
 export default router
