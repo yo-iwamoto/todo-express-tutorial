0b721bbabb7379c8953efdf92659e0c4bb72ea5f
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 11:29:47 +0900

    allow cors

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,13 @@ import auth from './routes/auth'
 const app = express();
 const env = process.env
 const PORT = env.PORT || 5000
-
 const router = express.Router();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
