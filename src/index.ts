a3fbad82af45974046609fb68bc32be98e17a01d
you-5805 <you.iwamoto918@gmail.com>
2021-03-25 23:02:23 +0900

    add origin

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
   }
+  res.header('Access-Control-Allow-Origin', "*")
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Token, Accept")
   res.header("Access-Control-Expose-Headers", "*")
